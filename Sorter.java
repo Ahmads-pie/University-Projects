6abc341b5f53fb374053b1df742fb79761011f88
Ahmad Algadhi <7moood100@gmail.com>
2023-09-25 14:24:25 +0300

    ss

--- Sorter.java
+++ Sorter.java
@@ -2,22 +2,7 @@ class Sorter {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
-
-    public static void selectionSort(int arr[]) {
-        // One by one move boundary of unsorted subarray
-        for (int i = 0; i < arr.length-1; i++)
-        {
-            // Find the minimum element in unsorted array
-            int min_idx = i;
-            for (int j = i+1; j < arr.length; j++)
-                if (arr[j] < arr[min_idx])
-                    min_idx = j;
-
-            // Swap the found minimum element with the first
-            // element
-            int temp = arr[min_idx];
-            arr[min_idx] = arr[i];
-            arr[i] = temp;
-        }
+    public static void hello() {
+        return;
     }
 }
