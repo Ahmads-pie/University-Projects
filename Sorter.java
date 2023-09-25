052c3a5096033e4f5e68e8fdbbcbc7e1322ec597
Mohammed ALmahana <s202064100@ccm.kfupm.edu.sa>
2023-09-25 14:12:21 +0300

    Insertion sort

--- Sorter.java
+++ Sorter.java
@@ -3,13 +3,14 @@ class Sorter {
         System.out.println("Hello, World!");
     }
 
-    public static void selectionSort(int arr[]) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            int min_idx = i;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[min_idx])
-                    min_idx = j;
+    public static void insertionSort(int array[]) {  
+        int n = array.length;  
+        for (int j = 1; j < n; j++) {  
+            int key = array[j];  
+            int i = j-1;  
+            while ( (i > -1) && ( array [i] > key ) ) {  
+                array [i+1] = array [i];  
+                i--;  
             }  
             int temp = arr[min_idx];
             arr[min_idx] = arr[i];
