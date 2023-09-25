809d9cdd3b16b3b66d5d6caf66a32583d7430ff4
Ahmad Algadhi <7moood100@gmail.com>
2023-09-25 14:03:28 +0300

    selection sort

--- Sorter.java
+++ Sorter.java
@@ -2,5 +2,18 @@ class Sorter {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
-    
+
+    public static void selectionSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+            }  
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
 }
