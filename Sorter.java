87e8f3e9ae81d9e106f346c0bdadba34fa6e1ad5
Ahmad Algadhi <7moood100@gmail.com>
2023-09-25 14:20:46 +0300

    Selection

--- Sorter.java
+++ Sorter.java
@@ -3,15 +3,18 @@ class Sorter {
         System.out.println("Hello, World!");
     }
 
-    public static void insertionSort(int array[]) {  
-        int n = array.length;  
-        for (int j = 1; j < n; j++) {  
-            int key = array[j];  
-            int i = j-1;  
-            while ( (i > -1) && ( array [i] > key ) ) {  
-                array [i+1] = array [i];  
-                i--;  
-            }  
+    public static void selectionSort(int arr[]) {
+        // One by one move boundary of unsorted subarray
+        for (int i = 0; i < arr.length-1; i++)
+        {
+            // Find the minimum element in unsorted array
+            int min_idx = i;
+            for (int j = i+1; j < arr.length; j++)
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+
+            // Swap the found minimum element with the first
+            // element
             int temp = arr[min_idx];
             arr[min_idx] = arr[i];
             arr[i] = temp;
